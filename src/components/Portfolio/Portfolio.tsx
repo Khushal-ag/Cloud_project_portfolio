6ffaf66ed4de1cf699c5aceff1c511e2245d860e
Khushal-ag <starkhush5@gmail.com>
2022-12-01 21:42:55 +0530

    init

---
+++ src/components/Portfolio/Portfolio.tsx
@@ -0,0 +1,169 @@
+import { Container } from "./styles";
+import githubIcon from "../../assets/github-icon.svg"
+import externalLinkIcon from "../../assets/external-link-icon.svg"
+import ScrollAnimation from "react-animate-on-scroll";
+
+
+export function Portfolio(){
+  return(
+    <Container id="portfolio">
+      <h2>Projects..</h2>
+
+      <div className="projects">
+
+      <ScrollAnimation animateIn="flipInX">
+        <div className="project">
+          <header>
+          <svg width="50" xmlns="http://www.w3.org/2000/svg" role="img" viewBox="0 0 24 24" fill="none" stroke="#23ce6b " stroke-width="1" stroke-linecap="round" stroke-linejoin="round" ><title>Folder</title><path d="M22 19a2 2 0 0 1-2 2H4a2 2 0 0 1-2-2V5a2 2 0 0 1 2-2h5l2 3h9a2 2 0 0 1 2 2z"></path></svg>
+          <div className="project-links">
+            <a href="https://github.com/cybergaz/Hyprland_Rice" target="_blank" rel="noreferrer">
+              <img src={githubIcon} alt="GitHub" />
+            </a>
+          </div>
+          </header>
+          <div className="body">
+            <h3>Hyprland_Rice</h3>
+            <p> It's my personal Linux Desktop Environment setup that i update over time depending on my usage. it is based on Arch Linux and uses hyprland as the window manager. 
+            </p>
+          </div>
+          <footer>
+            <ul className="tech-list">
+              <li>ArchLinux</li>
+              <li>Shell</li>
+              <li>Lua</li>
+            </ul>
+          </footer>
+        </div>
+      </ScrollAnimation>
+
+        <ScrollAnimation animateIn="flipInX">
+          <div className="project">
+            <header>
+            <svg width="50" xmlns="http://www.w3.org/2000/svg" role="img" viewBox="0 0 24 24" fill="none" stroke="#23ce6b " stroke-width="1" stroke-linecap="round" stroke-linejoin="round" ><title>Folder</title><path d="M22 19a2 2 0 0 1-2 2H4a2 2 0 0 1-2-2V5a2 2 0 0 1 2-2h5l2 3h9a2 2 0 0 1 2 2z"></path></svg>
+            <div className="project-links">
+              <a href="https://github.com/cybergaz/scripts/tree/main/wofi-emoji" target="_blank" rel="noreferrer">
+                <img src={githubIcon} alt="GitHub" />
+              </a>
+            </div>
+            </header>
+            <div className="body">
+              <h3>Wofi Emoji</h3>
+              <p> A simple script that uses wofi to search for emojis and copy them to the clipboard.
+              </p>
+            </div>
+            <footer>
+              <ul className="tech-list">
+                <li>Shell</li>
+              </ul>
+            </footer>
+          </div>
+        </ScrollAnimation>
+
+        <ScrollAnimation animateIn="flipInX">
+          <div className="project">
+            <header>
+            <svg width="50" xmlns="http://www.w3.org/2000/svg" role="img" viewBox="0 0 24 24" fill="none" stroke="#23ce6b " stroke-width="1" stroke-linecap="round" stroke-linejoin="round" ><title>Folder</title><path d="M22 19a2 2 0 0 1-2 2H4a2 2 0 0 1-2-2V5a2 2 0 0 1 2-2h5l2 3h9a2 2 0 0 1 2 2z"></path></svg>
+            <div className="project-links">
+              <a href="https://github.com/cybergaz/Kaoi" target="_blank" rel="noreferrer">
+              <img src={githubIcon} alt="GitHub" />
+              </a>
+            </div>
+            </header>
+            <div className="body">
+              <h3>Kaoi</h3>
+              <p> A Whatsapp bot fork from the original Kaoi bot, but with some modifications. 
+              </p>
+            </div>
+            <footer>
+              <ul className="tech-list">
+                <li>TypeScript</li>
+                <li>Heroku cloud</li>
+              </ul>
+            </footer>
+          </div>
+        </ScrollAnimation>
+
+        <ScrollAnimation animateIn="flipInX">
+          <div className="project">
+            <header>
+            <svg width="50" xmlns="http://www.w3.org/2000/svg" role="img" viewBox="0 0 24 24" fill="none" stroke="#23ce6b " stroke-width="1" stroke-linecap="round" stroke-linejoin="round" ><title>Folder</title><path d="M22 19a2 2 0 0 1-2 2H4a2 2 0 0 1-2-2V5a2 2 0 0 1 2-2h5l2 3h9a2 2 0 0 1 2 2z"></path></svg>
+            <div className="project-links">
+              <a href="https://github.com/cybergaz/heavens_anime" target="_blank" rel="noreferrer">
+              <img src={githubIcon} alt="GitHub" />
+              </a>
+            </div>
+            </header>
+            <div className="body">
+              <h3>Heavens Anime</h3>
+              <p>It's a in-development anime streaming website that i'm working on. its a group project btw and usage external api's to fetch anime data.
+              </p>
+            </div>
+            <footer>
+              <ul className="tech-list">
+                <li>ReactJs</li>
+                <li>JavaScript</li>
+                <li>Tailwind</li>
+              </ul>
+            </footer>
+          </div>
+        </ScrollAnimation>
+
+        <ScrollAnimation animateIn="flipInX">
+          <div className="project">
+            <header>
+            <svg width="50" xmlns="http://www.w3.org/2000/svg" role="img" viewBox="0 0 24 24" fill="none" stroke="#23ce6b " stroke-width="1" stroke-linecap="round" stroke-linejoin="round" ><title>Folder</title><path d="M22 19a2 2 0 0 1-2 2H4a2 2 0 0 1-2-2V5a2 2 0 0 1 2-2h5l2 3h9a2 2 0 0 1 2 2z"></path></svg>
+            <div className="project-links">
+              <a href="https://github.com/cybergaz/To_Do_List" target="_blank" rel="noreferrer">
+                <img src={githubIcon} alt="Visitar site" />
+              </a>
+            </div>
+            </header>
+            <div className="body">
+              <h3>To_Do_List</h3>
+              <p>A simple To Do list i made during my webd learning period. 
+              </p>
+            </div>
+            <footer>
+              <ul className="tech-list">
+                <li>HTML</li>
+                <li>CSS</li>
+                <li>JavaScript</li>
+              </ul>
+            </footer>
+          </div>
+        </ScrollAnimation>
+
+        <ScrollAnimation animateIn="flipInX">
+          <div className="project">
+            <header>
+            <svg width="50" xmlns="http://www.w3.org/2000/svg" role="img" viewBox="0 0 24 24" fill="none" stroke="#23ce6b " stroke-width="1" stroke-linecap="round" stroke-linejoin="round" ><title>Folder</title><path d="M22 19a2 2 0 0 1-2 2H4a2 2 0 0 1-2-2V5a2 2 0 0 1 2-2h5l2 3h9a2 2 0 0 1 2 2z"></path></svg>
+            <div className="project-links">
+              <a href="https://github.com/Khushal-ag/test_repo" target="_blank" rel="noreferrer">
+                <img src={githubIcon} alt="Visitor site" />
+              </a>
+              <a href="http://herokuapp.com" target="_blank" rel="noreferrer">
+                <img src={externalLinkIcon} alt="Visitar site" />
+              </a>
+            </div>
+            </header>
+            <div className="body">
+              <h3>Neet Coderz</h3>
+              <p>Just a simple webpage i built during my first year . just a nostalgic reminder for me : &#41;
+              </p>
+            </div>
+            <footer>
+              <ul className="tech-list">
+                <li>Html</li>
+                <li>Css</li>
+                <li>JavaSript</li>
+              </ul>
+            </footer>
+          </div>
+        </ScrollAnimation>
+
+
+
+      </div>
+    </Container>
+  );
+}
